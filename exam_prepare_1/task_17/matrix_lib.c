50d71e0d3f963480da0e385c2eefd2939be2a3f6
1 <ypopov2005@gmail.com>
2024-09-06 15:39:17 +0300

    temp

--- exam_prepare_1/task_17/matrix_lib.c
+++ exam_prepare_1/task_17/matrix_lib.c
@@ -9,16 +9,11 @@ void swap(int matrix[][MAX_SIZE], size_t i1, size_t j1, size_t i2, size_t j2)
 }
 
 // Поворот матрицы вправо
-/*void turn_right_90(int matrix[][MAX_SIZE], size_t count)
+void turn_right_90(int matrix[][MAX_SIZE], size_t size)
 {
-    for (size_t i = 0; i < count; i++)
-    {
-        for (size_t j = 0; j < count; j++)
-        {
-            swap(matrix, j, i, count - i - 1, j);
-        }
-    }
-}*/
+    transposition(matrix, size);
+    
+}
 
 void transposition(int matrix[][MAX_SIZE], size_t size)
 {
--- exam_prepare_1/task_17/matrix_lib.h
+++ exam_prepare_1/task_17/matrix_lib.h
@@ -3,4 +3,5 @@
 #include "constants.h"
 
 void transposition(int matrix[][MAX_SIZE], size_t size);
+void turn_right_90(int matrix[][MAX_SIZE], size_t size);
 #endif
