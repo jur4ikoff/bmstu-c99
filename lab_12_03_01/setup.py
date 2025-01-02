769fd630a7c25d8e476d06de6e2a6c56cacf39e4
Linux <ypopov2005@gmail.com>
2025-01-03 02:03:06 +0300

    this version dont compile

--- lab_12_03_01/setup.py
+++ lab_12_03_01/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup, Extension
 
 setup(name='my_arr_lib',
     ext_modules=[
-        Extension('my_arr_lib',
-            ['./myarrlib.c'],
-            include_dirs = ['.'],
+        Extension('libmy_arr',
+            ['./src/myarrlib.c'],
+            include_dirs = ['./inc/'],            
             define_macros = [('FOO','1')],
             undef_macros = ['BAR'],
-            library_dirs = ['.'],
-            libraries = ['example']
+            library_dirs = ['./out/'],
+            libraries = ['my_arr']
             )
         ]
 )
