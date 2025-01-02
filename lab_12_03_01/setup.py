94054233835b650698585eb75dd5b5ede16de17d
Linux <ypopov2005@gmail.com>
2025-01-03 02:24:34 +0300

    update readme and setup.py

--- lab_12_03_01/setup.py
+++ lab_12_03_01/setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup, Extension
 
-setup(name='my_arr_lib',
+setup(name='my_arr',
+    version='0.0.1',
+    description = "Test lib. I port functions from lab_12 to python with library and expansion module",
     ext_modules=[
-        Extension('libmy_arr',
+        Extension('my_arr_lib',
             ['./src/myarrlib.c'],
             include_dirs = ['./inc/'],            
             define_macros = [('FOO','1')],
