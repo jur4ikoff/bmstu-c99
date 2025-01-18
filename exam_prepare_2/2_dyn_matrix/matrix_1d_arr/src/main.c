a7999e7f6f4a039241518be6841b62e141875a3f
1 <ypopov2005@gmail.com>
2025-01-18 11:51:19 +0300

    add 1 method of dyn matrix

--- exam_prepare_2/2_dyn_matrix/matrix_1d_arr/src/main.c
+++ exam_prepare_2/2_dyn_matrix/matrix_1d_arr/src/main.c
@@ -5,7 +5,15 @@
 int main(void)
 {
     int rc = ERR_OK;
-    
-
+    matrix_t matrix = {0};
+    int n = 4, m = 2;
+    if ((rc = create_matrix(&matrix, n, m)) != ERR_OK)
+    {
+        free_matrix(&matrix);
+        return rc;
+    }
+    random_fill(&matrix);
+    print_matrix(matrix);
+    free_matrix(&matrix);
     return rc;
 }
--- exam_prepare_2/2_dyn_matrix/matrix_1d_arr/src/matrix.c
+++ exam_prepare_2/2_dyn_matrix/matrix_1d_arr/src/matrix.c
@@ -1,1 +1,62 @@
 #include "matrix.h"
+#include "constants.h"
+#include <stdlib.h>
+#include <time.h>
+
+err_t create_matrix(matrix_t *matrix, int n, int m)
+{
+    if (n < 1 || m < 1)
+    {
+        return ERR_SIZE;
+    }
+    if (matrix == NULL)
+        return ERR_ARGS;
+
+    matrix->matrix = malloc(sizeof(int) * n * m);
+    if (matrix->matrix == NULL)
+        return ERR_MEM_ALLOC;
+
+    matrix->n = (size_t)n;
+    matrix->m = (size_t)m;
+    return ERR_OK;
+}
+
+void free_matrix(matrix_t *matrix)
+{
+    if (matrix->matrix)
+    {
+        free(matrix->matrix);
+    }
+}
+
+void print_matrix(matrix_t matrix)
+{
+    if (matrix.n < 1 || matrix.m < 1)
+        return;
+
+    for (size_t i = 0; i < matrix.n; i++)
+    {
+        for (size_t j = 0; j < matrix.m; j++)
+        {
+            printf("%d ", matrix.matrix[i * matrix.n + j]);
+        }
+    }
+
+    printf("\n");
+}
+
+err_t random_fill(matrix_t *matrix)
+{
+    srand(time(NULL));
+    if (matrix->n < 1 || matrix->m < 1)
+        return ERR_ARGS;
+
+    for (size_t i = 0; i < matrix->n; i++)
+    {
+        for (size_t j = 0; j < matrix->m; j++)
+        {
+            matrix->matrix[i * matrix->n + j] = rand() % 256;
+        }
+    }
+    return ERR_OK;
+}
