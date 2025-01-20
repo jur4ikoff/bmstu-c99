ab6af3694f00c76ca7b8b7528abc2e19ed057656
Linux <ypopov2005@gmail.com>
2025-01-20 21:32:47 +0300

    add

--- exam_prepare_2/4_macros/src/main.c
+++ exam_prepare_2/4_macros/src/main.c
@@ -2,13 +2,29 @@
 #include <stdio.h>
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
-
+#define SCALE(x) ((x) * 10)
 #define PRINT_INT(n) printf(#n " = %d\n", (n))
+#define PRINT(s, ...) printf(s, __VA_ARGS__)
+
+#define PRICOL(x)         \
+    if (x > 5)            \
+    {                     \
+        PRINT_INT(x + 1); \
+    }                     \
+
+#define CR_INT(s) int i##s
 
 int main(void)
 {
     int rc = ERR_OK;
     int a = MAX(4, 5);
+    a = SCALE(a + 1);
     PRINT_INT(a);
+    PRICOL(a);
+    PRINT("test %d\n", 5);
+
+    CR_INT(1);
+    i1 = 1;
+    PRINT_INT(i1);
     return rc;
 }
